ffb22733b870f64ffb5ccf14755005ddffeabb75
m-vanspengen <m.vanspengen@student.avans.nl>
2024-11-20 15:27:52 +0100

    created all needed interfaces and gave dto's their own interface (used during mapping later)

--- TempleOfDoom/TempleOfDoom/DTO/RootDTO.cs
+++ TempleOfDoom/TempleOfDoom/DTO/RootDTO.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using TempleOfDoom.Interfaces;
 
 namespace TempleOfDoom.DTO
 {
-    public class RootDTO
+    public class RootDTO : IDTO
     {
         public RoomDTO[] rooms { get; set; }
         public ConnectionDTO[] connections { get; set; }
