96c6908977a3ea077753265c815db1f73f5a390f
m-vanspengen <m.vanspengen@student.avans.nl>
2024-11-26 15:12:47 +0100

    filereader done but not yet tested

--- TempleOfDoom/TempleOfDoom/DataLayer/LevelReader.cs
+++ TempleOfDoom/TempleOfDoom/DataLayer/LevelReader.cs
@@ -10,10 +10,23 @@ namespace TempleOfDoom.DataLayer
 {
     public class LevelReader : ILevelDataReader
     {
-
+        private RootDTO? RootDTO {  get; set; }
+        public LevelReader(string path)
+        {
+            RootDTO = readFile(path);
+        }
         public RootDTO readFile(string path)
         {
-            throw new NotImplementedException();
+            string? extension = Path.GetExtension(path)?.ToLower()?.TrimStart('.');
+            if (string.IsNullOrEmpty(extension)) throw new Exception("invalid path");
+            // determine reader type through reflection
+            string classname = "TempleOfDoom.DataLayer.ReaderStrategies." + char.ToUpper(extension[0]) + extension.Substring(1) + "LevelDataReader";
+            // check if type exists
+            Type? readerType = Type.GetType(classname);
+            if (readerType == null) throw new Exception(extension + " is not supported");
+            // create instance if exists
+            ILevelDataReader? levelDataReader = Activator.CreateInstance(readerType) as ILevelDataReader;
+            return levelDataReader.readFile(path);
         }
     }
 }
---
+++ TempleOfDoom/TempleOfDoom/DataLayer/ReaderStrategies/JsonLevelDataReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TempleOfDoom.DataLayer.DTO;
+using TempleOfDoom.Interfaces;
+
+namespace TempleOfDoom.DataLayer.ReaderStrategies
+{
+    public class JsonLevelDataReader : ILevelDataReader
+    {
+        public RootDTO readFile(string path)
+        {
+           {
+               try
+               {
+                    string json = File.ReadAllText(path);
+                    RootDTO root = JsonSerializer.Deserialize<RootDTO>(json);
+                    return root;
+               }
+               catch (Exception e)
+               {
+                    Console.WriteLine(e.Message);
+                    return null;
+               }
+           }
+        }
+    }
+}
