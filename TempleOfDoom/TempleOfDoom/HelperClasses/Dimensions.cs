3471ef5b192ae1f5c72b459084c4bd9dafbac8d7
m-vanspengen <m.vanspengen@student.avans.nl>
2024-11-28 18:57:07 +0100

    Create Dimensions.cs
    needed for room and room mapper

---
+++ TempleOfDoom/TempleOfDoom/HelperClasses/Dimensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempleOfDoom.HelperClasses
+{
+    public class Dimensions
+    {
+        private int width, height;
+        public Dimensions(int width, int height)
+        {
+            setDimensions(width, height);
+        }
+        public int getWidth()
+        {
+            return width;
+        }
+        public int getHeight()
+        {
+            return height;
+        }
+        public int[] getDimensions()
+        {
+            return [width, height];
+        }
+        public void setDimensions(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
